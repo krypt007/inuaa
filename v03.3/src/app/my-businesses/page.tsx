ffb4958ae62118b8077f6771553dd114b07944a1
krypt007 <steve@calstec.com>
2024-06-27 09:01:39 -0400

    update

--- v03.3/src/app/my-businesses/page.tsx
+++ v03.3/src/app/my-businesses/page.tsx
@@ -14,19 +14,19 @@ import { redirect } from "next/navigation";
 // import { authedOnly } from "@/app/actions/auth";
 
 export default async function MyBusinesses() {
-  const jwt = cookies().get("jwt");
-  if (!jwt?.value) {
-    return <MustLogin />;
-  }
+  // const jwt = cookies().get("jwt");
+  // if (!jwt?.value) {
+  //   return <MustLogin />;
+  // }
 
-  const authResult = await thirdwebAuth.verifyJWT({ jwt: jwt.value });
-  console.log({ authResult });
-  if (!authResult.valid) {    return <MustLogin />;  }
+  // const authResult = await thirdwebAuth.verifyJWT({ jwt: jwt.value });
+  // console.log({ authResult });
+  // if (!authResult.valid) {    return <MustLogin />;  }
 
-  // If the user has logged in, get their wallet address
-  const address = authResult.parsedJWT.sub;
-  console.log({ paredResult: authResult.parsedJWT });
-  if (!address) throw new Error("could not get wallet address");
+  // // If the user has logged in, get their wallet address
+  // const address = authResult.parsedJWT.sub;
+  // console.log({ paredResult: authResult.parsedJWT });
+  // if (!address) throw new Error("could not get wallet address");
 
   // This is the part that we do the gating condition.
   // If pass -> Allow them to access the page.
@@ -46,19 +46,19 @@ export default async function MyBusinesses() {
   );
 }
 
-const MustLogin = () => (
-  <div className="text-center">
-    You are not logged in. <br />
-    <a href="/" className="underline">
-      Log in now
-    </a>
-  </div>
-);
+// const MustLogin = () => (
+//   <div className="text-center">
+//     You are not logged in. <br />
+//     <a href="/" className="underline">
+//       Log in now
+//     </a>
+//   </div>
+// );
 
-const reason = "you do not own any NFT"; // replace this with your own reason
+// const reason = "you do not own any NFT"; // replace this with your own reason
 
-const NotAllowed = () => (
-  <div className="text-center">
-    You are logged in but you do not have access to this page because {reason}
-  </div>
-);
+// const NotAllowed = () => (
+//   <div className="text-center">
+//     You are logged in but you do not have access to this page because {reason}
+//   </div>
+// );
